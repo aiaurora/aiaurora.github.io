66eaadd94c4c44af550e51dacbd794eb2588ff51
Holmes Lin <41220173+aiaurora@users.noreply.github.com>
2022-01-29 23:27:36 +0800

    Update script.js

--- facerec/script.js
+++ facerec/script.js
@@ -172,9 +172,8 @@ function recognizeFaces(){
         })
 
     setInterval(async () => {
-      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+      const detections = await faceapi.detectAllFaces(video1, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
       const resizedDetections = faceapi.resizeResults(detections, displaySize)
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
       faceapi.draw.drawDetections(canvas, resizedDetections)
       faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
       faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
