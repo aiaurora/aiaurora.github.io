18fdac263262249eba770c82e1dac0c24dad3fd7
Holmes Lin <41220173+aiaurora@users.noreply.github.com>
2022-02-03 12:41:17 +0800

    Update script.js

--- mood/script.js
+++ mood/script.js
@@ -170,12 +170,12 @@ function recognizeFaces(){
                 url: "https://io.adafruit.com/api/v2/"+inputtextUser.value+"/feeds/gender/data?X-AIO-Key="+inputtext.value,
                 type: "POST",
                 data: {
-                  "value":gender
+                  "value":String(gender)
                 },
                 url: "https://io.adafruit.com/api/v2/"+inputtextUser.value+"/feeds/mood/data?X-AIO-Key="+inputtext.value,
                 type: "POST",
                 data: {
-                  "value":mood
+                  "value":String(mood)
                 },
               })
               
