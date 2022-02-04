4d755eadd46174e02cdedf3aed980e1880a77a3b
Holmes Lin <41220173+aiaurora@users.noreply.github.com>
2022-02-04 16:51:24 +0800

    Update script.js

--- facerec/script.js
+++ facerec/script.js
@@ -152,10 +152,18 @@ function recognizeFaces(){
               })
         */ 
             $.ajax({url: "https://io.adafruit.com/api/v2/"+inputtextUser.value+"/feeds/age/data?X-AIO-Key="+inputtext.value,
-                    data:{"value":parseInt(age)},type: "POST",success:CallBack,error:function(er){BackErr(er);}})
+                    data:{"value":parseInt(age)},
+                    type: "POST",
+                    success:CallBack,
+                    error:function(er){BackErr(er)}
+                   })
             console.log("age2 data  send to adafruit")
             $.ajax({url: "https://io.adafruit.com/api/v2/"+inputtextUser.value+"/feeds/gender/data?X-AIO-Key="+inputtext.value,
-                    data:{"value":gender},type: "POST",success:CallBack,error:function(er){BackErr(er);}})
+                    data:{"value":gender},
+                    type: "POST",
+                    success:CallBack,
+                    error:function(er){BackErr(er)}
+                   })
             console.log("gender2 data send to adafruit")
           
             end = start
